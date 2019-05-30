bd485fdc7cbcdbab065278057a2782d9df472c9e
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-30 13:26:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,6 +15,18 @@ def dictionary
 }
 end
 
-def word_substituter
-  
+def word_substituter(string)
+     #turn str to array using split
+   tweet = str.split(" ")
+   #iterate through each element of the str
+   new_tweet = tweet.collect do |word|
+      #if the dictionary's keys are in the str index
+      #return the value of the dictionary's keys instead of the original words
+      if dictionary.keys.include?(word.downcase) #downcase: return copy of upcase to downcase 
+         dictionary[word.downcase]
+      else
+         word #if not, just return the original words
+      end
+   end
+   new_tweet.join(" ") #turn array to string using join
 end
